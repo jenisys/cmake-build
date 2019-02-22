984d6256bf033fab0f875ca11eeb4b1459deb5ce
jenisys <jenisys@users.noreply.github.com>
2019-02-22 19:28:46 +0100

    BUMP-VERSION: 0.1.11 (was: 0.1.10)

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ def find_packages_by_root_package(where):
 # -----------------------------------------------------------------------------
 setup(
     name="cmake-build",
-    version="0.1.10",
+    version="0.1.11",
     description="cmake-build is a small wrapper around CMake to simplify its usage as build system",
     long_description=description,
     author="Jens Engel",
